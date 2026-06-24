85585480153786efa0e0b63be2daa25b5016dff2
matdombrock <dombrock.mathieu@gmail.com>
2026-06-24 06:41:56 -0700

    macro

--- ra-vcv/src/ra-macro.cpp
+++ ra-vcv/src/ra-macro.cpp
@@ -29,6 +29,18 @@ struct RaGnawbz1x4Module : Module {
         NUM_OUTPUTS
     };
     enum LightIds {
+        OUTPUT1_LED_R,
+        OUTPUT1_LED_G,
+        OUTPUT1_LED_B,
+        OUTPUT2_LED_R,
+        OUTPUT2_LED_G,
+        OUTPUT2_LED_B,
+        OUTPUT3_LED_R,
+        OUTPUT3_LED_G,
+        OUTPUT3_LED_B,
+        OUTPUT4_LED_R,
+        OUTPUT4_LED_G,
+        OUTPUT4_LED_B,
         NUM_LIGHTS
     };
 
@@ -40,12 +52,15 @@ struct RaGnawbz1x4Module : Module {
             configParam(ATTN1_PARAM + i, -1.f, 1.f, 1.f, string::f("Atten %d", i + 1));
             configParam(OFFSET1_PARAM + i, -5.f, 5.f, 0.f, string::f("Offset %d", i + 1), "V");
             configOutput(OUTPUT1 + i, string::f("Output %d", i + 1));
+            for (int c = 0; c < 3; c++)
+                configLight(OUTPUT1_LED_R + i * 3 + c, string::f("Output %d LED", i + 1));
         }
     }
 
     void process(const ProcessArgs &args) override {
         float macroV = params[MACRO_PARAM].getValue();
-        if (params[RANGE_PARAM].getValue() > 0.5f) {
+        float range = params[RANGE_PARAM].getValue();
+        if (range > 0.5f) {
             macroV *= 10.f;
         } else {
             macroV = macroV * 10.f - 5.f;
@@ -56,6 +71,40 @@ struct RaGnawbz1x4Module : Module {
             float offset = params[OFFSET1_PARAM + i].getValue();
             float v = macroV * attn + offset;
             outputs[OUTPUT1 + i].setVoltage(v);
+
+            float hue;
+            if (range > 0.5f) {
+                hue = clamp(v / 10.f, 0.f, 1.f);
+            } else {
+                hue = clamp((v + 5.f) / 10.f, 0.f, 1.f);
+            }
+            float s = 1.f, b = 1.f;
+            int hi = (int)(hue * 6.f);
+            float f = hue * 6.f - hi;
+            float p = b * (1.f - s);
+            float q = b * (1.f - f * s);
+            float t = b * (1.f - (1.f - f) * s);
+            float r, g;
+            switch (hi % 6) {
+                case 0: r = b; g = t; break;
+                case 1: r = q; g = b; break;
+                case 2: r = p; g = b; break;
+                case 3: r = p; g = q; break;
+                case 4: r = t; g = p; break;
+                default: r = b; g = p; break;
+            }
+            float bl;
+            switch (hi % 6) {
+                case 0: bl = p; break;
+                case 1: bl = p; break;
+                case 2: bl = t; break;
+                case 3: bl = b; break;
+                case 4: bl = b; break;
+                default: bl = q; break;
+            }
+            lights[OUTPUT1_LED_R + i * 3 + 0].setBrightness(r);
+            lights[OUTPUT1_LED_R + i * 3 + 1].setBrightness(g);
+            lights[OUTPUT1_LED_R + i * 3 + 2].setBrightness(bl);
         }
     }
 };
@@ -84,6 +133,7 @@ struct RaGnawbz1x4Widget : ModuleWidget {
                 addParam(createParamCentered<RaKnob>(Vec(cx, cy), module, RaGnawbz1x4Module::ATTN1_PARAM + i));
                 addParam(createParamCentered<RaKnobSmall>(Vec(cx, cy + 34), module, RaGnawbz1x4Module::OFFSET1_PARAM + i));
                 addOutput(createOutputCentered<RaPort>(Vec(cx, cy + 62), module, RaGnawbz1x4Module::OUTPUT1 + i));
+                addChild(createLightCentered<RaRGBLight>(Vec(cx, cy + 88), module, RaGnawbz1x4Module::OUTPUT1_LED_R + i * 3));
             }
         }
     }
