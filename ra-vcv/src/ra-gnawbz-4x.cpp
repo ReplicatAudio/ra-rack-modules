fe7efb1323a840c5637ebe462710f698dee0fe78
matdombrock <dombrock.mathieu@gmail.com>
2026-06-24 06:17:41 -0700

    gnawbz ui

--- ra-vcv/src/ra-gnawbz-4x.cpp
+++ ra-vcv/src/ra-gnawbz-4x.cpp
@@ -9,15 +9,12 @@ struct RaGnawbz4xModule : Module {
         KNOB1_PARAM,
         KNOB2_PARAM,
         KNOB3_PARAM,
-        KNOB4_PARAM,
         RANGE1_PARAM,
         RANGE2_PARAM,
         RANGE3_PARAM,
-        RANGE4_PARAM,
         SCALE1_PARAM,
         SCALE2_PARAM,
         SCALE3_PARAM,
-        SCALE4_PARAM,
         NUM_PARAMS
     };
     enum InputIds {
@@ -27,7 +24,6 @@ struct RaGnawbz4xModule : Module {
         OUTPUT1,
         OUTPUT2,
         OUTPUT3,
-        OUTPUT4,
         NUM_OUTPUTS
     };
     enum LightIds {
@@ -40,15 +36,12 @@ struct RaGnawbz4xModule : Module {
         LIGHT3_R,
         LIGHT3_G,
         LIGHT3_B,
-        LIGHT4_R,
-        LIGHT4_G,
-        LIGHT4_B,
         NUM_LIGHTS
     };
 
     RaGnawbz4xModule() {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             configParam(KNOB1_PARAM + i, 0.f, 1.f, 0.5f, string::f("Knob %d", i + 1));
             configSwitch(RANGE1_PARAM + i, 0.f, 1.f, 0.f, string::f("Range %d", i + 1), {"\u00B15V", "0\u201310V"});
             configParam(SCALE1_PARAM + i, 0.f, 1.f, 1.f, string::f("Scale %d", i + 1), "%", 0.f, 100.f);
@@ -59,7 +52,7 @@ struct RaGnawbz4xModule : Module {
     }
 
     void process(const ProcessArgs &args) override {
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             float v = params[KNOB1_PARAM + i].getValue();
             if (params[RANGE1_PARAM + i].getValue() > 0.5f) {
                 v *= 10.f;
@@ -116,14 +109,14 @@ struct RaGnawbz4xWidget : ModuleWidget {
         addChild(createWidget<RaScrew>(Vec(0, box.size.y - RACK_GRID_WIDTH)));
         addChild(createWidget<RaScrew>(Vec(box.size.x - RACK_GRID_WIDTH, box.size.y - RACK_GRID_WIDTH)));
 
-        float y[] = {32, 120, 208, 296};
+        float y[] = {54, 159, 264};
 
-        for (int i = 0; i < 4; i++) {
-            addParam(createParamCentered<RaKnob>(Vec(box.size.x / 2, y[i]), module, RaGnawbz4xModule::KNOB1_PARAM + i));
-            addParam(createParamCentered<RaSwitch2>(Vec(box.size.x - 8, y[i] + 18), module, RaGnawbz4xModule::RANGE1_PARAM + i));
-            addParam(createParamCentered<RaKnobSmall>(Vec(box.size.x / 2, y[i] + 30), module, RaGnawbz4xModule::SCALE1_PARAM + i));
-            addOutput(createOutputCentered<RaPort>(Vec(box.size.x / 2, y[i] + 54), module, RaGnawbz4xModule::OUTPUT1 + i));
-            addChild(createLightCentered<RaRGBLight>(Vec(8, y[i] + 45), module, RaGnawbz4xModule::LIGHT1_R + i * 3));
+        for (int i = 0; i < 3; i++) {
+            addParam(createParamCentered<RaKnob>(Vec(18, y[i]), module, RaGnawbz4xModule::KNOB1_PARAM + i));
+            addParam(createParamCentered<RaKnobSmall>(Vec(18, y[i] + 36), module, RaGnawbz4xModule::SCALE1_PARAM + i));
+            addOutput(createOutputCentered<RaPort>(Vec(18, y[i] + 64), module, RaGnawbz4xModule::OUTPUT1 + i));
+            addChild(createLightCentered<RaRGBLight>(Vec(48, y[i] + 2), module, RaGnawbz4xModule::LIGHT1_R + i * 3));
+            addParam(createParamCentered<RaSwitch2>(Vec(48, y[i] + 26), module, RaGnawbz4xModule::RANGE1_PARAM + i));
         }
     }
 };
